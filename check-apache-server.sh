8b0210ed2d2b40a8410b13dd1e77f940c4c4af34
viridityforever <viridityforever@users.noreply.github.com>
2016-12-15 15:58:21 -0800

    Assignment: cron job FILE 1 OF 2

--- check-apache-server.sh
+++ check-apache-server.sh
@@ -13,8 +13,6 @@
 #Your script should be croned to run once an hour and email you if there is no activity on your server and no one logged in.
 
 #HERE IS THE SCRIPT FOR THIS FILE OF THE ASSIGNMENT:
-# The  -l  option causes the current crontab to be displayed on standard output.
-#crontab -l
 #number of users logged in is $users
 users=$( /usr/bin/who | grep -c "" )
 #count the number of apache users (1 if apache is up, 2 if not), store as $apachestatus
@@ -22,10 +20,10 @@ apachestatus=$( ps auxw | grep apache | grep -c "")
 # if there isn't an apahche user AND there aren't any users logged in
 if [ "$apachestatus" -eq "0" ] && [ "$users" -eq "0" ];
 then
-        echo “ Yikes, server down. The aws apache server with ip address @54.86.131.96 is DOWN. Also, there no one is logged on to the server. Put on your cape and fix it. ” | mail -s "Chronjob test" viridityforever@gmail.com
+        echo “ Yikes, the aws apache server is DOWN. Put on your cape and fix it. ” | mail -s "Server is down: apache AWS" viridityforever@gmail.com
        #echo “This is a message” | mail -s “This is a subject” youremail@example.com
 #else
-	#echo "Server is up" | mail -s "Chronjob test - server is up line" viridityforever@gmail.com
+	#echo "Server is up" | mail -s "Chronjob test - server is up" viridityforever@gmail.com
 fi
 
 # Ensure that you make this script executable by entering the following in the command line
