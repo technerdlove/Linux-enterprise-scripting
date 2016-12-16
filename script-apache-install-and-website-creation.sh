ec4e0b6b239210eb4b91662785c998de14988be5
viridityforever <viridityforever@users.noreply.github.com>
2016-12-15 18:21:56 -0800

    Assignment: apache install and website creation

---
+++ script-apache-install-and-website-creation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "installing apache server"
+sudo yum -y install httpd
+
+echo "enabling apache server"
+sudo systemctl enable httpd.service
+
+echo "starting apache server"
+sudo systemctl start httpd.service
+
+echo "If you open the security settings for port 80 on your server, you should see the apache start page"
+
+#landingpage.html is a static HTML page that the user created and 
+#saved the  home directory of the user
+echo "add static HTML page to apache server"
+sudo sh -c 'cat landingpage.html > /var/www/html/landingpage.html'
