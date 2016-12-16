5d84bc2a527df122e65496fa28f6d9b096018b10
viridityforever <viridityforever@users.noreply.github.com>
2016-12-15 16:50:28 -0800

    Assignment: translate shell scripts in to python

--- translate-shell-scripts-to-python.py
+++ translate-shell-scripts-to-python.py
@@ -5,28 +5,28 @@ import os
 def install_django():
     print('installing django')
 
-def install_pip:
-    os.system('sudo yum -y install python-pip')
- 
-def dir_create:
-     os.system('sudo mkdir /opt/django')
-     os.system('sudo chown -R ect-user /opt/django')
+    def install_pip():
+        os.system('sudo yum -y install python-pip')
 
-def install_virtualenv:
-    os.system('sudo pip install virtualenv')
-    os.system('virtualenv django-env')
+    def dir_create():
+         os.system('sudo mkdir /opt/django')
+         os.system('sudo chown -R ect-user /opt/django')
 
- 
-def activate_virtualenv:
-    os.system('source /opt/django/django-env/bin/activate')
-    
+    def install_virtualenv():
+        os.system('sudo pip install virtualenv')
+        os.system('virtualenv django-env')
 
-    os.system(' virtualenv django-env')
-    os.system('pip install django')
-    os.system('sudo django-admin startporject project1')
-    print('installing django')       
-    print('running server')
-    os.system('python manage.py runserver 0.0.0.0:8000');
-    os.system(' https://docs.djangoproject.com/en/1.10/intro/tutorial01/')
+
+    def activate_virtualenv():
+        os.system('source /opt/django/django-env/bin/activate')
+
+
+        os.system(' virtualenv django-env')
+        os.system('pip install django')
+        os.system('sudo django-admin startporject project1')
+        print('installing django')       
+        print('running server')
+        os.system('python manage.py runserver 0.0.0.0:8000');
+        os.system(' https://docs.djangoproject.com/en/1.10/intro/tutorial01/')
 
 install_django()    
