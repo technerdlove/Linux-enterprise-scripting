96552cc042962d104f6fe812348958fd66caa080
viridityforever <viridityforever@users.noreply.github.com>
2016-12-20 20:47:22 -0800

    Assignment: final
    truncated to just install django

--- install-django-server-from-command-line.py
+++ install-django-server-from-command-line.py
@@ -26,12 +26,10 @@ def launch_test_instance():
       KeyName = amazon_pem_key,
       SecurityGroupIds = firewall_profiles,
       UserData="""#!/usr/bin/python
-
-
 # The purpose of this script is to configure and install a django server without needing to enter sudo
-
 # This first line is very important.  You should import os first because of the numerous system calls in the script.
-import os
+import os, sys
+# This installs apache web server
 def install_apache():
 	print('installing apache server')
 	os.system('sudo yum -y install httpd')
@@ -41,35 +39,6 @@ def install_apache():
 	os.system('sudo systemctl start httpd.service')
 	print('If you open the security settings for port 80 on your server, you should see the apache start page')
 install_apache()
-
-# Here the script installs the django server with python and virtualenv
-def install_django():
-	print('installing django')
-install_django()
-def install_pip():
-	os.system('yum -y install python-pip')
-	print('installing python-pip')
-install_pip()
-def install_virtualenv():
-	os.system('cd /opt')
-	os.system('pip install virtualenv')
-	os.system('virtualenv django-env')
-	print('installing virtual-env')
-install_virtualenv()
-
-# Here the script activates the django server
-def activate_virtualenv():
-	os.system('source /opt/django/django-env/bin/activate')
-	os.system('which python')
-	os.system('pip install django')
-	os.system('django-admin --version')
-	os.system('django-admin startproject project1; exists_ok=true')
-	os.system('tree project1')
-	print('installing tree')
-	print('installing django')
-	os.system('python /opt/django/project1/ manage.py runserver 0.0.0.0:8000&')
-	print('done')
-activate_virtualenv()
 
 # This installs git and the tree package that belongs with git
 def install_git():
@@ -82,20 +51,41 @@ def install(package):
 	os.system('yum -y install '+package)
 install('tree')
 
-# This installs the email service
-def mailx():
-	print('installing mailx')
-	os.system('yum -y install mailx')
-mailx()
+# Here the script installs the django server with python and virtualenv
+def install_django():
+	print('installing django')
+	os.system('python --version')
+      	os.system('rpm -iUvh https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm')
+install_django()
+def install_pip():
+	os.system('yum -y install python-pip')
+	print('installing python-pip')
+install_pip()
+def install_virtualenv():
+	os.system('cd /opt')
+	os.system('pip install virtualenv')
+	os.system('cd /opt')
+      	os.system('mkdir django')
+      	os.system('sleep 5')
+      	os.system('cd django')
+	os.system('virtualenv django-env')
+	print('installing virtual-env')
+install_virtualenv()
 
-# This installs security patches - specifically the dirty cow patch
-#  This is just for notes - WILL NOT CALL IN SCRIPT FOR TESTING. REBOOTS SERVER, BUT FOR TEST WE WANT SERVER DEPLOYED
-def kernel():
-	print('installing kernel')
-	os.system('yum -y clean all && yum update kernel -y && yum -y reboot')
-def verifying_dirty_cow():
-	os.system('rpm -q --changelog kernel | grep CVE-2016-5195')
-	
+# Here the script activates the django server
+def activate_virtualenv():
+	os.system('source /opt/django/django-env/bin/activate')
+	os.system('which python')
+	os.system('chown -R ec2-user /opt/django')
+	os.system('pip install django')
+	os.system('django-admin --version')
+	os.system('django-admin startproject project1; exists_ok=true')
+	os.system('tree project1')
+	print('installing tree')
+	print('installing django')
+	os.system('python /opt/django/project1/manage.py runserver 0.0.0.0:8000&')
+	print('done')
+activate_virtualenv()
 	"""
 	)
 
